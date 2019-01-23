9c7a1b4ef6208799dc4f92d7c5fa581a0e6e7868
Sytze Simonse <simonse.sytze@gmail.com>
2019-01-23 21:22:04 +0100

    JS-functie voor afspelen Morse toegevoegd. Map gemaakt voor sources.

--- javascript/general.js
+++ javascript/general.js
@@ -4,5 +4,18 @@ function checkFullscreen() {
       alert('Browser is in fullscreen');
   } else {
     alert('Browser is not in fullscreen yet');
-  }  
+  }
 }
+
+// Play Morse-sound every X sec.
+function playMorse(intervalMinutes,audioFile) {
+  var intervalID = window.setInterval(playSound, 1000 * 60 * intervalMinutes);
+
+  function playSound() {
+    var audio = new Audio(audioFile);
+    audio.play();
+  }
+}
+
+// Play morse every 2 minutes.
+playMorse(0.5,"sources/morse.wav");
